4d0613210545c5e6a9c538331764ddf93bff43b7
romain <romain.comtet@epitech.eu>
2020-04-07 15:41:32 +0200

    cdscd

--- src/main.c
+++ src/main.c
@@ -5,24 +5,14 @@
 ** main.c
 */
 
-<<<<<<< HEAD
 #include "my.h"
 #include "lemin.h"
-=======
-#include"my.h"
-#include"lemin.h"
->>>>>>> 9ad2b2f84268778d1c118a9c2e7277d38c979476
 
 int main(int ac, char **av)
 {
     args_t args;
     node_t *tmp;
-<<<<<<< HEAD
     
-=======
-    array_t *all = NULL;
-
->>>>>>> 9ad2b2f84268778d1c118a9c2e7277d38c979476
     recover_all(&args);
     tmp = init_all_node_tunnel(args);
     
